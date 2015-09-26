fe37f58146154dc4c8043d6c1e619c7574c81f11
Briana Chapman <bri.chapman@gmail.com>
2015-09-25 20:47:30 -0500

    Initial Commit

---
+++ HueDance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HueDance
+//
+//  Created by Bri Chapman on 9/25/15.
+//  Copyright © 2015 com.bchapman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
